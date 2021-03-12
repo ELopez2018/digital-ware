cfeb7829ae2975772e6b6048d6785b04f20d1ceb
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2021-03-12 17:25:35 -0500

    Servicios

---
+++ src/app/core/services/passenger.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { PersonalDataModel } from '@models/personal-data.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PassengerService {
+  access_token: string = '';
+  token = '';
+  urlApi: string = environment.API_URL;
+  Headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient) {}
+
+  // private httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     Authorization: 'Bearer ' + this.token,
+  //   }),
+  // };
+
+  // public setHttpOption() {
+  //   this.httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type': 'application/json',
+  //       Authorization: 'Bearer ' + this.token,
+  //     }),
+  //   };
+  // }
+
+  createPassenger$(Data: PersonalDataModel): Observable<PersonalDataModel[]> {
+    return this.http
+      .post<PersonalDataModel[]>(this.urlApi + `requestServices`, Data)
+      .pipe(
+        tap((resp) => {
+          return resp;
+        })
+      );
+  }
+  searchAllPassenger$(
+    Data: PersonalDataModel
+  ): Observable<PersonalDataModel[]> {
+    return this.http
+      .get<PersonalDataModel[]>(this.urlApi + `requestServices`)
+      .pipe(
+        tap((resp) => {
+          return resp;
+        })
+      );
+  }
+  updatePassenger$(Data: PersonalDataModel): Observable<PersonalDataModel[]> {
+    return this.http
+      .put<PersonalDataModel[]>(this.urlApi + `requestServices`, Data)
+      .pipe(
+        tap((resp) => {
+          return resp;
+        })
+      );
+  }
+  deletePassenger$(Data: PersonalDataModel): Observable<PersonalDataModel[]> {
+    return this.http
+      .delete<PersonalDataModel[]>(this.urlApi + `requestServices`)
+      .pipe(
+        tap((resp) => {
+          return resp;
+        })
+      );
+  }
+}
